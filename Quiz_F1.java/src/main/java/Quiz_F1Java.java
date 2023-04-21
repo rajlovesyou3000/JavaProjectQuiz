0114b03e056c741d54233c88e2dddb0e80c57c7f
rajlovesyou3000 <116373624+rajlovesyou3000@users.noreply.github.com>
2023-04-21 11:32:49 +0530

    Add files via upload

---
+++ Quiz_F1.java/src/main/java/Quiz_F1Java.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author 91735
+ */
+public class Quiz_F1Java {
+
+    public static void main(String[] args) {
+        Frame2 sc=new Frame2();
+        sc.show();
+        
+        
+        
+        
+    }
+}
